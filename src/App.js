9ae93e07c345b1ab24b6c72c6b3080aec0342749
Enes KURU <enes793k@icloud.com>
2018-10-02 11:02:34 +0300

    fetch with the axios library example added.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: [],
+    isLoading: true
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      /*fetch('https://jsonplaceholder.typicode.com/users').then(data => data.json()).then(users => {
+        this.setState({users, isLoading: false});
+      })*/
+      axios.get('https://jsonplaceholder.typicode.com/users').then(response => response.data).then(users => {
+        this.setState({users, isLoading: false});
+      })
+    }, 2000);
+  }
+
+
   render() {
+    const {isLoading} = this.state;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Users</h1>
+        {isLoading ? 'Loading...' : ''}
+        {
+          !isLoading ? this.state.users.map(user =>
+            <div key={user.id} className={"userList"}>{user.name} - @{user.username}</div>
+          ) : null
+        }
       </div>
     );
   }
